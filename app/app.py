6f49aea3a772461863804156827b0b15b51a2477
ReiJr <reinaldo.isj@hotmail.com>
2020-06-03 14:57:47 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello Nalds!</h3>" \
+    html = "<h3>Vai tomar no cu bolsonaro</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" 
 
     return html.format(hostname=socket.gethostname())
